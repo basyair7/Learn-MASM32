5f5ee03de51b0fbc5692c8903db76d0a63bd4fc1
basyair7 <basyair.fathul@gmail.com>
2024-01-28 02:00:11 +0700

    add lesson 5

--- basic/lesson1/howToCompileASM.asm
+++ basic/lesson1/howToCompileASM.asm
@@ -1,6 +1,6 @@
 ; run on command prompt
 ; select on directory masm32 compile
 
-ml /c /Zd /coff "namefile.asm"
-link /SUBSYSTEM:CONSOLE "namefile.obj"
-namefile
+; ml /c /Zd /coff "namefile.asm"
+; link /SUBSYSTEM:CONSOLE "namefile.obj"
+; namefile
