62860c6bbc30d85f7dc31fbb3ad655ca2a0d4a73
basyair7 <basyair.fathul@gmail.com>
2024-01-28 02:03:28 +0700

    add lesson 5

--- basic/lesson1/howToCompileASM.asm
+++ basic/lesson1/howToCompileASM.asm
@@ -1,7 +1,7 @@
 ; 1. run on command prompt
 ; 2. select on directory masm32 compile
 ; 3. create new folder in directory /masm32/newfolder/
-; 4. paste code on newfolder
+; 4. paste code .asm in newfolder
 ; 5. run on cmd : 
 ; > ml /c /Zd /coff "namefile.asm"
 ; > link /SUBSYSTEM:CONSOLE "namefile.obj"
