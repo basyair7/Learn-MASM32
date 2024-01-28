5c3439a7e8113b543400009f18b0290f185c5a82
basyair7 <basyair.fathul@gmail.com>
2024-01-28 11:16:11 +0700

    revisi lesson1

--- basic/lesson1/helloworld.asm
+++ basic/lesson1/helloworld.asm
@@ -1,4 +1,4 @@
-name "helloworld.asm" ; file name "hello world :p"
+name "helloworld2.asm" ; file name "hello world :p"
 .386
 .model flat,stdcall
 option casemap:none
@@ -14,14 +14,14 @@ includelib \masm32\lib\masm32.lib
 ; deklarasikan variabel
 .data
     HELLOWORLDBABY db "Halo dunia!", 0
-    ENDPRINT db " ", 1
+    NEWLINE db 13, 10, 0
 
 .code
 
 ; start program
 start:
     invoke StdOut, addr HELLOWORLDBABY
-    invoke StdOut, addr ENDPRINT
+    invoke StdOut, addr NEWLINE
     invoke ExitProcess, 0
 
 end start ; end program
